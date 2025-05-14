529040c7396180c11df9208e97d4104f939a38e3
TuNombre <tuemail@gmail.com>
2025-05-14 15:28:20 -0500

    Ejercici testin terminado

--- src/Test/Calculadora.java
+++ src/Test/Calculadora.java
@@ -1,4 +1,26 @@
 package Test;
 
 public class Calculadora {
+
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public int restar(int a, int b) {
+        return a - b;
+    }
+
+    // Método para verificar si un número es par
+    public boolean esPar(int numero) {
+        return numero % 2 == 0;
+    }
+
+    // Método para calcular el factorial de un número
+    public int factorial(int n) {
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
 }
--- src/Test/CalculadoraTest.java
+++ src/Test/CalculadoraTest.java
@@ -1,4 +1,37 @@
 package Test;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class CalculadoraTest {
+
+    private Calculadora calc;
+
+    @Before
+    public void setUp() {
+        calc = new Calculadora();
+    }
+
+    @Test
+    public void testRestar() {
+        int resultado = calc.restar(4, 3);
+        assertEquals("ese resultado deberia dar 1", 1, resultado);
+    }
+
+    @Test
+    public void testEsPar() {
+        assertTrue("2 debería ser par", calc.esPar(2));
+        assertFalse("7 no debería ser par", calc.esPar(7));
+        assertTrue("6 debería ser par", calc.esPar(6));
+        assertTrue("12 debería ser par", calc.esPar(12));
+    }
+
+    @Test
+    public void testFactorial() {
+        assertEquals("0! debe dar: 1", 1, calc.factorial(0));
+        assertEquals("3! debe dar: 6", 6, calc.factorial(3));
+        assertEquals("5! debe dar: 120", 120, calc.factorial(5));
+    }
 }
